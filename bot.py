b92ca4a28053ae6dfd93f14164be8e8da59847d3
Vitalij <belikovvitalij16@gmail.com>
2024-05-03 23:28:21 +0300

    БАн по соо

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
@@ -22,5 +27,19 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+@bot.message_handler(func=lambda message: True)
+def ban_for_message(message):
+    if "https://" in message.text:
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        username = message.from_user.username
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id)
+            bot.reply_to(message, f"Пользователь @{username} был забанен.")
+
 
 bot.infinity_polling(none_stop=True)
